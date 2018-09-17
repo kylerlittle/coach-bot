17731b50976740dfbda4b5385a4d91c7a46928ee
kylerlittle <kyler.little@wsu.edu>
2018-09-17 12:19:46 -0700

    messed around with spacy nlp and actually got amazing results

---
+++ coach-bot/actions/actions.py
@@ -0,0 +1,11 @@
+
+# Generic Action class
+class Action:
+    # Function members
+    
+    # Execute the action here
+    # (): void
+    def execute(self):
+        print("yay execution")
+        
+# Action subclasses here...
---
+++ coach-bot/nlp/parsed_to_action.py
@@ -0,0 +1,5 @@
+import spacy
+
+MODEL_LOCATION = './model'
+
+# Could probably just use functions in this file, but might be a good opportunity to use an anti-pattern!
