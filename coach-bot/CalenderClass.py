a176f11e4e80bf379463426a847f1c512c5d5631
Christopher Young <Young.christopher.425@gmail.com>
2018-09-24 22:45:50 -0700

    Calender class with basic functions

---
+++ coach-bot/CalenderClass.py
@@ -0,0 +1,38 @@
+import datetime
+from CalenderClassTest import *
+from datetime import timedelta
+
+class Calendar:
+	_events = []
+	_user = None
+	def __init__(self,user):
+		self._user = user
+	def __str__(self):
+		print(self._events)
+	def getUser(self):
+		return self._user
+	def addToCalendar(self, evnt):
+		#if not isinstance(evnt, Event):
+		if False:
+			print ('Unexpected argument type\n\tExpected type: Event | Got Type: ' + str(type(evnt))) 
+		else:
+			inserted = False
+			for i in range(0,len(self._events)-1):
+				if (((evnt._start-datetime.datetime.now()).days
+							 > (self._events[i]._start-datetime.datetime.now()).days)
+					and ((evnt._start-datetime.datetime.now()).days
+							 > (self._events[i+2]._start-datetime.datetime.now()).days)):
+					self._events.insert(i+1,evnt)
+					inserted = True
+					break
+			if not inserted:
+				self._events.append(evnt)
+	def showWorkouts(self):
+		for i in events:
+			if (isinstance(i, Workout)):
+				print(i)
+	def showWorkoutsDay(self,day):
+		for i in events:
+			if (isinstance(i, Workout) and (i._start == day)):
+				print(i)
+
---
+++ coach-bot/CalenderClassTest.py
@@ -0,0 +1,45 @@
+import datetime
+from CalenderClass import *
+
+class MockUser:
+	def __init__(self,name):
+		self._name = name
+	def __str__(self):
+		print(self._name)
+
+class Event:
+	def __init__(self,name,start):
+		self._name=name
+		self._start=start
+	def __str__(self):
+		print('Event:' + self._name + '    ' + self._start)
+
+
+class Workout(Event):
+	def __init__(self,name,start):
+		Event.__init__(self,name,start)
+	def __str__(self):
+		return ('Workout:' + self._name + '    ' + str(self._start))
+
+class TestCalendar:
+	def setup(self):
+		self.user = None
+		self.user = MockUser('TestUser') 
+		self.cal = None
+		self.cal = Calendar(self.user)
+
+	def addToCalendarTest(self):
+		self.setup()
+		self.cal.addToCalendar(Workout('Legs',datetime.datetime(2018,11,9,0,0)))
+		self.cal.addToCalendar(Workout('Arms',datetime.datetime(2018,9,19,0,0)))
+		self.cal.addToCalendar(Workout('Chest',datetime.datetime(2018,10,10,0,0)))
+		self.cal.addToCalendar(Workout('Tris',datetime.datetime.now()))
+		for i in self.cal._events:
+			print(str(i))	
+		print('Expected results \"Tris,Arms,Chest,Legs\"') 
+def main():
+	tclass = TestCalendar()
+	tclass.addToCalendarTest()
+
+if __name__ == "__main__":
+	main()	
