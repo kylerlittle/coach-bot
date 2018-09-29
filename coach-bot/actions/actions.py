b09feaf51165cd4c280edd9af5e3b7c9fd07e52a
Shusanta Bhattarai <shusantabhattarai@Shusantas-MacBook-Pro.local>
2018-09-29 15:39:19 -0700

    added some functionality, need to implement calendar

--- coach-bot/actions/actions.py
+++ coach-bot/actions/actions.py
@@ -5,12 +5,18 @@
   with the user before doing something.
 > This is a big TODO.
 """
-
-
+import sys
+import calendar from CalendarClass
+sys.path.append(/Users/shusantabhattarai/coach-bot/coach-bot/CalendarClass.py)
+from CalendarClass import Calendar
 # Generic Action class
+ 
 class Action:
     def __init__(self, _details={}):
-        self.details = _details
+        self.current_calories = 0
+        self.current_stats = 25
+        self.calendar = Calendar()
+
 
     # Function members
     
@@ -22,35 +28,32 @@ class Action:
 # Action subclasses here...
 class HelpAction(Action):
     def execute(self):
-        print(self.details)
-        print("HELP ME")
+        print( "Set your calorie intake and chose a workout!")
 
-class ScheduleWorkoutAction(Action):
+class ScheduleWorkoutAction(Action): ''' need help from chris '''
     def execute(self):
         print(self.details)
-        print("Scheduling workout")
-
+        
 class DisplayCalendarAction(Action):
     def execute(self):
         print(self.details)
         print("Displaying calendar")
 
-class DisplayWorkoutStatsAction(Action):
+class DisplayWorkoutStatsAction(Action): 
     def execute(self):
-        print(self.details)
-        print("Displaying workout stats")
+        print(self.current_stats)
 
 class DisplayWorkoutScheduleAction(Action):
     def execute(self):
         print(self.details)
-        print("Displaying workout schedule")
+        self.calendar.showWorkouts()
 
 class SetFeedbackAction(Action):
     def execute(self):
         print(self.details)
-        print("Setting feedback")
+        print("Setting feedback") 
 
 class SetCaloriesAction(Action):
     def execute(self):
         print(self.details)
-        print("Setting calories")
+        self.calories = input("Setting the calorie intake:")
