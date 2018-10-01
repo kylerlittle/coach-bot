4387d8f597db0acc22357e96ffce4402e7a68d7c
Christopher Young <Young.christopher.425@gmail.com>
2018-09-30 21:05:39 -0700

    Modified variable names to match action class. Added function to search for event by id. Modified string function.

--- coach-bot/CalendarClass.py
+++ coach-bot/CalendarClass.py
@@ -8,7 +8,10 @@ class Calendar:
 	def __init__(self,user):
 		self._user = user
 	def __str__(self):
-		print(self._events)
+		s = ""
+		for i in self._events:
+			s += str(i)
+		return s
 	def getUser(self):
 		return self._user
 	def addToCalendar(self, evnt):
@@ -19,9 +22,9 @@ class Calendar:
 			inserted = False
 			for i in range(0,len(self._events)-1):
 				if (((evnt._start-datetime.datetime.now()).days
-							 > (self._events[i]._start-datetime.datetime.now()).days)
-					and ((evnt._start-datetime.datetime.now()).days
-							 > (self._events[i+2]._start-datetime.datetime.now()).days)):
+							 > (self._events[i]._startDateTime-datetime.datetime.now()).days)
+					and ((evnt._startDateTime-datetime.datetime.now()).days
+							 > (self._events[i+2]._startDateTime-datetime.datetime.now()).days)):
 					self._events.insert(i+1,evnt)
 					inserted = True
 					break
@@ -33,8 +36,17 @@ class Calendar:
 				print(i)
 	def showWorkoutsDay(self,day):
 		for i in events:
-			if (isinstance(i, Workout) and (i._start == day)):
+			if (isinstance(i, Workout) and (i._startDateTime == day)):
 				print(i)
+	def showEventsDay(self,day):
+		for i in events:
+			if i._startDateTime == day)):
+				print(i)
+	def findEvent(self,iden):
+		for i in events:
+			if i._id == iden:
+				return i
+		return 0
 	def saveCalendar(self):
 		#TODO: SAVE CALENDAR AS XML
 		pass
