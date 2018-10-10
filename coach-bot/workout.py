25f35d7e7077c0f9483ae734d7cd38eae519fbf5
cwright <connor.d.wright@wsu.edu>
2018-10-09 18:53:01 -0700

    added setters and getters

--- coach-bot/workout.py
+++ coach-bot/workout.py
@@ -5,22 +5,45 @@ import datetime
 
 class Workout(Event):
 
-    def __init__(self):
+    def __init__(self, id = 0, calendarId = 0, 
+    description = "", location = "", startDateTime = datetime.datetime.now(), 
+    endDateTime = datetime.datetime.now(), user = User(), feedback = "", ):
         self._feedback = FeedBack()
         self._calories = 0
-        self.tags = [""]
+        self._tags = [""]
         Event.__init__(self)
+        Event.Event(id, calendarId, description, location, startDateTime, endDateTime, user)
 
+    def Workout(self, calories = 0, tags = [""]):
+        self._feedback = feedback
+        self._calories = calories
+        self._tags = tags
+        
     def Workout(self, id = 0, calendarId = 0, 
     description = "", location = "", startDateTime = datetime.datetime.now(), 
     endDateTime = datetime.datetime.now(), user = User(), feedback = "", calories = 0, tags = [""]):
         self._feedback = feedback
         self._calories = calories
-        self.tags = tags
+        self._tags = tags
         Event.Event(id, calendarId, description, location, startDateTime, endDateTime, user)
 
     def setFeedBack(self, feedBack):
         self._feedback(feedBack)
+
+    def getFeedback(self):
+        return self._feedback
+
+    def setCalories(self, calories):
+        self._calories = calories
+
+    def getCalories(self):
+        return self._calories
+
+    def setTags(self, tags):
+        self._tags = tags
+
+    def getTags(self):
+        return self._tags
 
     def __str__(self):
         return "----------\n feedback: {0}\n calories: {1}\n tags: {2}".format(self._feedback, self._calories, self._tags)
