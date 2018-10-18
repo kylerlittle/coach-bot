ed4f579c58af4192fcca581a39d1cf8ce81b55ad
kylerlittle <kyler.little@wsu.edu>
2018-10-17 20:19:09 -0700

    imports all good now; start test code

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,4 +1,9 @@
 import unittest
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../coach-bot'
+# Make sure our src code directory is in PATH variable.
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
--- tests/nlp/test_tokens_to_action.py
+++ tests/nlp/test_tokens_to_action.py
@@ -1,7 +1,12 @@
 import unittest
+nlp = __import__("coach-bot.nlp.tokens_to_action")  # import it this way since hyphenated
 
 class TestTokensToAction(unittest.TestCase):
     def setUp(self):
+        # String for each use case & variations.
+        pass
+
+    def test_process_input(self):
         pass
 
     def test_return_tokenized(self):
@@ -14,6 +19,21 @@ class TestTokensToAction(unittest.TestCase):
         pass
     
     def test_get_datetime_from_str(self):
+        pass
+
+    def test_get_root_action(self):
+        pass
+
+    def test_get_action_receiver(self):
+        pass
+
+    def test_get_action_receiver_attrs(self):
+        pass
+
+    def test_initially_process_toks(self):
+        pass
+
+    def test_tokens_to_action(self):
         pass
 
     def tearDown(self):
